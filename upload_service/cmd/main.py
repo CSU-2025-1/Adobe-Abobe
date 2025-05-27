03071acc9f9815a080fa98969e0b8280a35d594a
dellxdesq <nikita673551@gmail.com>
2025-05-27 14:57:35 +0300

    Feat: ускорение сервиса по загрузке фотографий

--- upload_service/cmd/main.py
+++ upload_service/cmd/main.py
@@ -1,19 +1,22 @@
 import asyncio
 import logging
-
-from internal.broker.rabbitclient.workers import wrap_consumer
-from internal.broker.rabbitclient.workers import consume_images_data
+from internal.repository.s3_repo import ensure_bucket_exists
+from internal.broker.rabbitclient.workers import wrap_consumer, consume_images_data
 
 logging.basicConfig(
-    level=logging.INFO,  # или INFO
+    level=logging.INFO,
     format="%(asctime)s [%(levelname)s] %(message)s",
 )
 
+CONSUMER_COUNT = 10
+
 async def serve():
-
-
+    await ensure_bucket_exists()
     await asyncio.gather(
-        wrap_consumer(consume_images_data, "consume_images_data"),
+        *[
+            wrap_consumer(consume_images_data, f"consume_images_data_{i}")
+            for i in range(CONSUMER_COUNT)
+        ]
     )
 
 if __name__ == "__main__":
