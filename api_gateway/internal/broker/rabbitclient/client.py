66901f82e321f2ddb5c2303db0fd64077e9a261d
Igor <IgorLom2003@mail.ru>
2025-05-28 00:53:21 +0500

    Feat: ограничил очереди рэббита, написал рпс метод для стори-сервиса

--- api_gateway/internal/broker/rabbitclient/client.py
+++ api_gateway/internal/broker/rabbitclient/client.py
@@ -1,8 +1,17 @@
+import asyncio
+import json
+import logging
 import aio_pika
+from aio_pika.abc import AbstractIncomingMessage
+
 from config.config import cfg
 
 RABBIT_URL = f"amqp://{cfg.RABBITMQ_USER}:{cfg.RABBITMQ_PASS}@{cfg.RABBITMQ_HOST}:{cfg.RABBITMQ_PORT}/"
-_connection = None
+
+_connection: aio_pika.RobustConnection | None = None
+_channel: aio_pika.Channel | None = None
+_callback_queue: aio_pika.Queue | None = None
+_pending_futures: dict[str, asyncio.Future] = {}
 
 
 async def get_connection():
@@ -13,5 +22,36 @@ async def get_connection():
 
 
 async def get_channel():
-    connection = await get_connection()
-    return await connection.channel()
+    global _channel
+    if _channel is None or _channel.is_closed:
+        conn = await get_connection()
+        _channel = await conn.channel()
+        await _channel.set_qos(prefetch_count=10)
+    return _channel
+
+
+async def setup_callback_queue():
+    global _callback_queue
+
+    if _callback_queue:
+        return
+
+    channel = await get_channel()
+    _callback_queue = await channel.declare_queue(exclusive=True, auto_delete=True)
+
+    async def on_response(msg: AbstractIncomingMessage):
+        correlation_id = msg.correlation_id
+        future = _pending_futures.pop(correlation_id, None)
+
+        if future:
+            try:
+                decoded = json.loads(msg.body.decode())
+                future.set_result(decoded)
+                logging.debug(f"response for {correlation_id}")
+            except Exception as e:
+                future.set_exception(e)
+
+        await msg.ack()
+
+    await _callback_queue.consume(on_response, no_ack=False)
+    logging.info(f"Callback queue initialized: {_callback_queue.name}")
--- api_gateway/internal/broker/rabbitclient/producers.py
+++ api_gateway/internal/broker/rabbitclient/producers.py
@@ -3,45 +3,34 @@ import base64
 import json
 import logging
 import uuid
-
 import aio_pika
 
-from internal.broker.rabbitclient.client import get_channel
+from internal.broker.rabbitclient.client import get_channel, setup_callback_queue, _pending_futures, _callback_queue
 from internal.core.entity.auth.auth_dto import AuthRequest
 from internal.core.entity.upload.upload_dto import UploadRequest
 from internal.core.entity.filter.filter_dto import FilterRequest
+from internal.core.entity.story.story_dto import StoryRequest
 
 VALIDATION_QUEUE = "validation"
 AUTHORIZATION_QUEUE = "authorization"
 UPLOAD_IMAGE_QUEUE = "upload"
 FILTER_QUEUE = "filter"
+STORY_QUEUE = "filter_story"
 
 
-async def publish_rpc(routing_key: str, payload: dict, timeout: float = 30.0):
+async def publish_rpc(routing_key: str, payload: dict, timeout: float = 5.0):
+    await setup_callback_queue()
     channel = await get_channel()
-    callback_queue = await channel.declare_queue(exclusive=True, auto_delete=True)
+
     correlation_id = str(uuid.uuid4())
+    future = asyncio.get_running_loop().create_future()
+    _pending_futures[correlation_id] = future
 
-    future = asyncio.get_event_loop().create_future()
-
-    async def on_response(message: aio_pika.IncomingMessage):
-        logging.info(f"📨 Received response on callback_queue: correlation_id={message.correlation_id}")
-        if message.correlation_id == correlation_id:
-            try:
-                decoded = json.loads(message.body.decode())
-                future.set_result(decoded)
-                logging.info(f"✅ Matched response: {decoded}")
-            except Exception as e:
-                logging.error(f"❌ Failed to decode response: {e}")
-            await message.ack()
-
-    await callback_queue.consume(on_response)
-
-    logging.info(f"📤 Publishing to {routing_key}, waiting reply on {callback_queue.name}")
+    logging.debug(f"📤 Publishing to {routing_key}, correlation_id={correlation_id}")
     await channel.default_exchange.publish(
         aio_pika.Message(
             body=json.dumps(payload).encode(),
-            reply_to=callback_queue.name,
+            reply_to=_callback_queue.name,
             correlation_id=correlation_id,
         ),
         routing_key=routing_key,
@@ -50,6 +39,7 @@ async def publish_rpc(routing_key: str, payload: dict, timeout: float = 30.0):
     try:
         return await asyncio.wait_for(future, timeout=timeout)
     except asyncio.TimeoutError:
+        await _pending_futures.pop(correlation_id, None)
         raise Exception("RPC timeout")
 
 
@@ -79,6 +69,14 @@ async def send_upload_message(upload_request: UploadRequest):
         "user_id": upload_request.user_id,
     }
     return await publish_rpc(UPLOAD_IMAGE_QUEUE, payload)
+
+
+async def send_story_message(story_request: StoryRequest):
+    payload = {
+        "user_id": story_request.user_id,
+        "timestamp": story_request.timestamp
+    }
+    return await publish_rpc(STORY_QUEUE, payload)
 
 
 async def send_filters_message(filter_request: FilterRequest):
